4346a3068c3f0a15fb1b820f8579bdb983fc7b0e
Yuliya Muni <yuliyahism@gmail.com>
2020-02-18 22:53:29 +0300

    changes5

--- index.js
+++ index.js
@@ -4,12 +4,12 @@ const age = prompt('Сколько вам лет?');
 
 let access='';
 
-if (age > 18) {
-    access = "Доступ разрешен";
-} else if (age < 18 || age >= 16 && age != 0) {
-    access = "Доступ запрещен. Приходи через пару лет";
-} else {
-    access = "Доступ запрещен.";
-}
+// if (age > 18) {
+//     access = "Доступ разрешен";
+// } else if (age < 18 || age >= 16 && age != 0) {
+//     access = "Доступ запрещен. Приходи через пару лет";
+// } else {
+//     access = "Доступ запрещен.";
+// }
 
 document.write(`<h4>${access}</h4>`);
