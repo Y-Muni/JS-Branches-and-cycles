99f28222151c76ec4443f4721b34c998d1eecd9c
Yuliya Muni <yuliyahism@gmail.com>
2020-02-19 18:57:36 +0300

    changes2

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 document.write("<h1>Hello from JS</h1>");
 
-const age = prompt('Сколько вам лет?');
+// const age = prompt('Сколько вам лет?');
 
-let access = (age > 18) ? 'Доступ разрешен' : 'Доступ запрещен';
+// let access = (age > 18) ? 'Доступ разрешен' : 'Доступ запрещен';
 
 // if (age > 18) {
 //     access = "Доступ разрешен";
@@ -12,4 +12,13 @@ let access = (age > 18) ? 'Доступ разрешен' : 'Доступ зап
 //     access = "Доступ запрещен.";
 // }
 
-document.write(`<h4>${access}</h4>`);
+// document.write(`<h4>${access}</h4>`);
+
+// Циклы
+let num = 0;
+
+while (num <= 10) {
+    
+}
+
+document.write(`<h4>${num}</h4>`);
